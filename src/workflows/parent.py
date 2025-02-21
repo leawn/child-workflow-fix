601d38456b3b022a3ac8cd99c5fc85369d78259d
leawn <leonrubner2809@gmail.com>
2025-02-21 12:51:35 +0100

    Add error handling

--- src/workflows/parent.py
+++ src/workflows/parent.py
@@ -1,7 +1,7 @@
 from restack_ai.workflow import workflow, log, workflow_info
 from pydantic import BaseModel
 from .child import ChildWorkflow, ChildInput
-
+from datetime import timedelta
 class ParentInput(BaseModel):
     child: bool = True
 
@@ -15,15 +15,27 @@ class ParentWorkflow:
         if input.child:
             # use the parent run id to create child workflow ids
             parent_workflow_id = workflow_info().workflow_id
-
-            log.info("Start ChildWorkflow and dont wait for result")
-            result = await workflow.child_start(ChildWorkflow, input=ChildInput(name="world"), workflow_id=f"{parent_workflow_id}-child-start")
-            
-            log.info("Start ChildWorkflow and wait for result")
-            result = await workflow.child_execute(ChildWorkflow, input=ChildInput(name="world"), workflow_id=f"{parent_workflow_id}-child-execute")
-            log.info("ChildWorkflow completed", result=result)
-            return ParentOutput(result="ParentWorkflow completed")
-        
+            try:
+                log.info("Start ChildWorkflow and dont wait for result")
+                result = await workflow.child_start(
+                    ChildWorkflow,
+                    input=ChildInput(name="world"),
+                    workflow_id=f"{parent_workflow_id}-child-start",
+                    execution_timeout=timedelta(seconds=30)
+                )
+                log.info("Start ChildWorkflow and wait for result")
+                result = await workflow.child_execute(
+                    ChildWorkflow,
+                    input=ChildInput(name="world"),
+                    workflow_id=f"{parent_workflow_id}-child-execute",
+                    execution_timeout=timedelta(seconds=30)
+                )
+            except Exception as e:
+                log.info("ChildWorkflow failed", error=e)
+                raise e
+            else:
+                log.info("ChildWorkflow completed", result=result)
+                return ParentOutput(result="ParentWorkflow completed")
         else:
             log.info("ParentWorkflow without starting or executing child workflow")
             return ParentOutput(result="ParentWorkflow completed")
